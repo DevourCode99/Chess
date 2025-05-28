774f18c9a089e053e8e6c7c51d3f76e7cd61b290
DevourCode99 <matt@madisonwi.co>
2025-05-28 15:46:43 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+import { showStartMenu } from './components/startMenu.js';
+import { BoardUI }       from './components/board.js';
 
+// 1) Ask for desired strength
+showStartMenu((skill) => {
+  // 2) Launch worker & configure engine
+  const engine = new Worker('./engine/worker.js', { type: 'module' });
+  engine.postMessage('uci');
+  engine.postMessage(`setoption name Skill Level value ${skill}`);
+
+  // 3) Fire up UI
+  new BoardUI(engine);
+});
