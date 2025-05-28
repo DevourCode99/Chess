186197f6d686bb93a8f9ead322bc05011d18a68d
DevourCode99 <matt@madisonwi.co>
2025-05-28 15:46:27 -0500

    Update worker.js

--- src/engine/worker.js
+++ src/engine/worker.js
@@ -1,1 +1,8 @@
+/**
+ * Wrapper worker → Stockfish WASM (bundled file).
+ * Accepts and relays raw UCI strings.
+ */
+importScripts('https://cdn.jsdelivr.net/npm/stockfish.wasm@0.10.0/stockfish.js');
 
+self.onmessage = (e) => stockfish.postMessage(e.data);
+stockfish.onmessage = (e) => postMessage(e.data);
